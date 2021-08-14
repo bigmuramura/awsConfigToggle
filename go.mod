60b4fb8256c74834daf324a8a87999e164d59943
Ohmura Yasutaka <ohmura.yasutaka@classmethod.jp>
2021-08-14 23:23:29 +0900

    Config On 単一リージョンで実行可能

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/bigmuramura/awsConfigToggle
 
 go 1.16
 
-require github.com/aws/aws-sdk-go v1.40.22 // indirect
+require github.com/aws/aws-sdk-go v1.40.22
--- main.go
+++ main.go
@@ -5,16 +5,65 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/costexplorer"
+	"github.com/aws/aws-sdk-go/service/configservice"
+	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
 func main() {
-	fmt.Println("test")
-
+	// Region
 	REGION := "ap-northeast-1"
 	sess := session.Must(session.NewSession())
-	svc := costexplorer.New(
+	svc := ec2.New(
 		sess,
 		aws.NewConfig().WithRegion(REGION))
+
+	resultRegions, err := svc.DescribeRegions(nil)
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	var regionNames []string
+	for _, regions := range resultRegions.Regions {
+		regionName := *regions.RegionName
+		regionNames = append(regionNames, regionName)
+	}
+	listStatus(regionNames)
+	configOn()
+}
+
+func listStatus(regions []string) {
+	for _, region := range regions {
+		sess := session.Must(session.NewSession())
+		svc := configservice.New(
+			sess,
+			aws.NewConfig().WithRegion(region))
+
+		input := &configservice.DescribeConfigurationRecorderStatusInput{}
+
+		result, err := svc.DescribeConfigurationRecorderStatus(input)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(region, *result.ConfigurationRecordersStatus[0].Recording)
+	}
+}
+
+func configOn() {
+	region := "ap-northeast-1"
+	sess := session.Must(session.NewSession())
+	svc := configservice.New(
+		sess,
+		aws.NewConfig().WithRegion(region))
+
+	input := &configservice.StartConfigurationRecorderInput{
+		ConfigurationRecorderName: aws.String("default"),
+	}
+
+	result, err := svc.StartConfigurationRecorder(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(result)
 
 }
